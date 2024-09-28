75ad828476376619d46431f178ed573f84f1baf7
Luis Solano <luisfer.sm15@gmail.com>
2024-09-28 08:48:10 -0600

    estructura de carpetas

---
+++ src/assets/animations/index.ts
@@ -0,0 +1,5 @@
+import DeliveryAnimation from './DeliveryAnimation.json'
+import LoaderCircleAnimation from './LoaderCircleAnimation.json'
+import LocationAnimation from './LocationAnimation.json'
+
+export { DeliveryAnimation, LoaderCircleAnimation, LocationAnimation }
